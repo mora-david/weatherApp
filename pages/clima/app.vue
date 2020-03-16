6ed029433a701a243f444b8200010f574fe1e43d
david.mora <david.89.mora@gmail.com>
2020-03-16 10:42:52 -0600

    first

---
+++ pages/clima/app.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+<div class="mt-5">prueba</div>
+<div>
+    <h5>Selecciona el estado</h5>
+<select name="estados">
+
+
+    <option v-for="estado in estados" :key="estado.id">{{estado}}</option>
+
+</select>
+<h5>Selecciona la ciudad</h5>
+
+{{cities}}
+
+<i class="wi size-5rem wi-rain" ></i>
+
+</div>
+
+
+{{pruebaclima}}
+  </div>
+</template>   
+
+
+
+
+<script>
+import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+export default {
+  data(){
+    return{
+        cities:'',
+      pruebaclima: 'nada',
+      estados:['Aguascalientes','Baja California','Baja California Sur','Campeche','Coahuila de Zaragoza','Colima','Chiapas','Chihuahua','Distrito Federal','Durango','Guanajuato','Guerrero','Hidalgo','Jalisco','México','Michoacán de Ocampo','Morelos','Nayarit','Nuevo León','Oaxaca','Puebla','Querétaro','Quintana Roo','San Luis Potosí','Sinaloa','Sonora','Tabasco','Tamaulipas','Tlaxcala','Veracruz de Ignacio de la Llave','Yucatán','Zacatecas'],
+      cities:''
+
+  }},
+  created(){
+    this.getwh()
+    this.getcities()
+  },
+  methods:{
+getwh() {
+    
+      const url = 'http://api.openweathermap.org/data/2.5/weather?q=london&appid=74809c652a768472e95f5d913143d783'
+      const params={
+          id:'2172797',
+          appid:'74809c652a768472e95f5d913143d783'
+      }
+      axios
+        .get(url,params)
+        .then((response) => {
+          this.pruebaclima = response.data
+        })
+        .catch(() => {
+          alert('error')
+        })
+    },
+
+  
+  getcities() {
+      const url = 'https://inegifacil.com/cities/3'
+      axios
+        .get(url)
+        .then((response) => {
+          this.cities = response.data
+        })
+        .catch(() => {
+          alert('error')
+        })
+    },
+}}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div>
+        <button @click="getwh()">presiona</button>
+      <p>{{pruebaclima}}</p>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      prueba:'esto es 1111una prueba',
+      pruebaclima: 'nada',
+      pruebaclima1: ''
+    }
+
+  },
+  created(){
+    //this.getwh()
+  },
+  methods:{
+getwh() {
+      const url = 'api.openweathermap.org/data/2.5/weather?id=2172797&appid=74809c652a768472e95f5d913143d783'
+      axios
+        .get(url)
+        .then((response) => {
+          this.pruebaclima = response.data
+        })
+        .catch(() => {
+          alert('error')
+        })
+    },
+    
+  },
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
